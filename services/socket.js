3ee1dc757c29cc8b32cbdee7e7db63997ace4168
Doğukan <dogu_kutluay_1999@hotmail.com>
2022-03-10 23:55:48 +0300

    kadirin socket ile imtihanı

--- services/socket.js
+++ services/socket.js
@@ -1,33 +1,26 @@
 let users = [];
-const addUser = (userId, socketId) => {
-  !users.some((user) => user.userId === userId) &&
-    users.push({ userId, socketId });
+const addUser = user => {
+  if (user.userId === null || user.socketId === null) return;
+  !users.some(userI => userI.userId === user.userId) && users.push(user);
 };
-const removeUser = (socketId) => {
-  users = users.filter((user) => user.socketId !== socketId);
+const removeUser = socketId => {
+  users = users.filter(user => user.socketId !== socketId);
 };
-const getUser = (userId) => {
-  return users.find((user) => user.userId === userId);
+const getUser = userId => {
+  return users.find(user => user.userId === userId);
 };
+
 export default function socket({ io }) {
-  io.on("connection", (socket) => {
-    //burada userı ekliyoruz socket id ile her sayfa yeniledniğinde socket ıd değişir
-    // bunu clientta ben yazdım örnek olarak
-    socket.on("addUser", (userId) => {
-      addUser(userId, socket.id);
-      console.log(userId + "added");
+  io.on('connection', socket => {
+    socket.on('addUser', user => {
+      addUser(user);
+      console.log(users);
     });
-    //burada userı mesaj göndereceği kişinin idsini ve ne gönderdiyse onu gönderiyorum
-    // const user göndereceğim kişinin idsi demektir, yukarıda tuttuğum array içerisinde socket id ile to vasıtasyıla iletyorum böylelikle sadece o dinleyebilecek
 
-    //getMessage ı clientta dinlediğimde socket.on() ile eğer socket idim eşit ise zaten yakalarım değilse o mesage gelmeyecektir
-    //send message her mesaj gönderdiğinde kullanacaksın
-    socket.on("sendMessage", ({ sender, receiverId, conversationId, text }) => {
+    socket.on('sendMessage', ({ sender, receiverId, conversationId, text }) => {
       const user = getUser(receiverId);
-
-      console.log(user);
       if (user) {
-        io.to(user.socketId).emit("getMessage", {
+        io.to(user.socketId).emit('getMessage', {
           sender,
           conversationId,
           text,
@@ -35,8 +28,7 @@ export default function socket({ io }) {
       }
     });
 
-    //burada da disconnect oldğunda çıkartıyorum kullancıyı ki sorun yaşamasın
-    socket.on("disconnect", () => {
+    socket.on('disconnect', () => {
       removeUser(socket.id);
     });
   });
