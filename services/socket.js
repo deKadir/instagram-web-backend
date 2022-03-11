350082fb155de1e3320fae510a904630b706018e
kadir <develioglu306@gmail.com>
2022-03-11 14:26:18 +0300

    last commit

--- services/socket.js
+++ services/socket.js
@@ -1,26 +1,25 @@
 let users = [];
-const addUser = user => {
+const addUser = (user) => {
   if (user.userId === null || user.socketId === null) return;
-  !users.some(userI => userI.userId === user.userId) && users.push(user);
+  !users.some((userI) => userI.userId === user.userId) && users.push(user);
 };
-const removeUser = socketId => {
-  users = users.filter(user => user.socketId !== socketId);
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
 };
-const getUser = userId => {
-  return users.find(user => user.userId === userId);
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
 };
 
 export default function socket({ io }) {
-  io.on('connection', socket => {
-    socket.on('addUser', user => {
+  io.on("connection", (socket) => {
+    socket.on("addUser", (user) => {
       addUser(user);
-      console.log(users);
     });
 
-    socket.on('sendMessage', ({ sender, receiverId, conversationId, text }) => {
+    socket.on("sendMessage", ({ sender, receiverId, conversationId, text }) => {
       const user = getUser(receiverId);
       if (user) {
-        io.to(user.socketId).emit('getMessage', {
+        io.to(user.socketId).emit("getMessage", {
           sender,
           conversationId,
           text,
@@ -28,7 +27,7 @@ export default function socket({ io }) {
       }
     });
 
-    socket.on('disconnect', () => {
+    socket.on("disconnect", () => {
       removeUser(socket.id);
     });
   });
