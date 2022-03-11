ed3680bb58238c60d4289907255c5953d5b95f3a
kadir <develioglu306@gmail.com>
2022-03-11 16:30:30 +0300

    unlike error removed

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -26,21 +26,20 @@ export const likeComment = asyncErrorWrapper(async (req, res, next) => {
 export const likePost = asyncErrorWrapper(async (req, res, next) => {
   const { postId } = req.params;
   const user = req.user.id;
-  const like = await Like.findOne({ user, id: postId }).catch(async () => {
+  const like = await Like.findOne({ user, id: postId }).catch(() => {});
+  if (like) {
+    like.remove();
+    res.status(200).json({
+      error: false,
+      message: "unlike",
+    });
+  } else {
     await Like.create({ user, type: "Post", id: postId }).then((like) =>
       res.status(200).json({
         error: false,
         message: "like",
       })
     );
-  });
-  if (like) {
-    like.remove();
-    like.save();
-    res.status(200).json({
-      error: false,
-      message: "unlike",
-    });
   }
 });
 export const getPostLikes = asyncErrorWrapper(async (req, res, next) => {
